084491f7d9a1e36ddfe382f61615cc7d6b8a311c
hieupth <64821726+hieupth@users.noreply.github.com>
2023-12-28 16:56:22 +0700

    fix: return mean pooling

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import os
 import json
+import torch
 import ovmsclient
 import numpy as np
-from typing import Union
+from os import environ
 from fastapi import FastAPI
 from hftokenizer import BertTokenizer, PhobertTokenizer
 
+environ["GRPC_DNS_RESOLVER"] = "native"
 # Read tokenizer name.
 TOKENIZER = os.getenv("TOKENIZER", "phobert")
 # Is words segment?
@@ -33,6 +35,11 @@ client = ovmsclient.make_grpc_client(os.getenv("OVMS_URI", "localhost:9000"))
 # Make fastapi
 app = FastAPI()
 
+def mean_pooling(model_output, attention_mask):
+    token_embeddings = model_output #First element of model_output contains all token embeddings
+    input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+    return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+
 @app.post("/")
 async def predict(data: dict):
   tokenized = tokenizer(data["message"])
@@ -41,4 +48,8 @@ async def predict(data: dict):
   for k in metadata["inputs"].keys():
     inputs.update({k: np.array([tokenized[k]], dtype=np.int64)})
   res = client.predict(model_name=data["model"], inputs=inputs)
+  res = mean_pooling(
+    torch.from_numpy(np.asarray(res)), 
+    torch.from_numpy(np.asarray(tokenized["attention_mask"]))
+  )
   return json.dumps(res.squeeze().tolist())
